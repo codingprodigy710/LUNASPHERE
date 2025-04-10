f2b33c7b2f852e8aa4f89b022171753bd8935516
Ivan <georgeivan554@gmail.com>
2025-04-10 21:59:36 +0530

    heheheheheh

---
+++ lib/screens/tabs/upload_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UploadTab extends StatelessWidget {
+  const UploadTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Upload New Photo', style: TextStyle(color: Colors.white)),
+    );
+  }
+}
