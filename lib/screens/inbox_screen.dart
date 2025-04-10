4c1a2d3d510afd84451ffeafbae8a8e1d67462b0
Ivan <georgeivan554@gmail.com>
2025-04-10 22:39:40 +0530

    Messages button added

---
+++ lib/screens/inbox_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class InboxScreen extends StatelessWidget {
+  const InboxScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Inbox',
+          style: TextStyle(color: Colors.white),
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: const Center(
+        child: Text(
+          'Your messages will appear here 👽',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/tabs/home_tab.dart
+++ lib/screens/tabs/home_tab.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 import '../../widgets/post_card.dart';
+import '../inbox_screen.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: ListView(
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        title: const Text(
+          'Lunasphere',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.message_outlined, color: Colors.white),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const InboxScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+
+      body: ListView(
         children: const [
           PostCard(
             username: 'moonwalker42',
